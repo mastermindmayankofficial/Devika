19d4c5ab7504999edbf3d8819dce792e62d0ca65
AS TG <126592884+TGxIRFAN@users.noreply.github.com>
2024-02-02 20:43:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import logging
 import logging.config
+import os
+import sys
+from os import environ
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
@@ -9,7 +12,8 @@ from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 from aiohttp import web
-from plugins.web_support import web_server
+from plugins import web_server
+PORT = environ.get("PORT", "8080")
 
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
