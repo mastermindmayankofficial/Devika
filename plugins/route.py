147370a1c15e684270d926987b5a13866d83e6ba
AS TG <126592884+TGxIRFAN@users.noreply.github.com>
2024-02-02 20:50:31 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -1,8 +1,7 @@
 from aiohttp import web
-from .route import routes
 
+routes = web.RouteTableDef()
 
-async def web_server():
-    web_app = web.Application(client_max_size=30000000)
-    web_app.add_routes(routes)
-    return web_app 
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("Anandhu")
