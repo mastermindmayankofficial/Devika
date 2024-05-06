ad4a486da6f3b474224e2fe0e402efced2258fed
mastermindmayank <88104889+mastermindmayankofficial@users.noreply.github.com>
2024-05-06 13:11:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,8 +41,8 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'NOTQF')
 
 # MongoDB information
-DATABASE_URI = "mongodb+srv://Hemanth:Hemanth@cluster0.uzzh3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-DATABASE_NAME = "cluster0"
+DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_NAME = environ.get('DATABASE_NAME', "MASTERMIND")
 COLLECTION_NAME = "test_devika1"
 
 
